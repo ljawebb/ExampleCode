42b1840e040f7fec287e79cfbe791c3e488ec253
Laurence <lw17773@my.bristol.ac.uk>
2018-03-28 13:15:58 +0100

    Updated neillncurses description

--- Difussion Limited Aggregation/neillncurses.h
+++ Difussion Limited Aggregation/neillncurses.h
@@ -1,9 +1,10 @@
+/* A wrapper for the ncurses library */
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <time.h>
 #include <ncurses.h>
-#include <unistd.h>
 
 #define KEY_ESC 27
 
